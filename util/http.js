736896a74fa01e49fec477b4e9837cebebcf761b
WIN-6UU7GKRPQQR\Administrator <250809345@qq.com>
2019-03-15 18:02:26 +0800

    调整

--- util/http.js
+++ util/http.js
@@ -22,11 +22,11 @@ class HTTP {
             success: (res) => {
                 let code = res.statusCode.toString()
                 if (code.startsWith('2')) {
-                    if(res.data.error_code!=0){
+                    if (res.data.error_code != 0) {
                         let error_code = res.data.error_code
                         this._show_error(error_code)
                     }
-                    params.success(res.data)
+                    params.success && params.success(res.data)
                 }
                 else {
                     let error_code = res.data.error_code
